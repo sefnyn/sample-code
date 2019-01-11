0865adb1e38020b2b4374293a95978d5752c24bb
Nick Syrotiuk <sefnyn@gmail.com>
2019-01-11 17:19:04 +0000

    Update create-default.6.x.py

--- api/v2010/message/create-default/create-default.6.x.py
+++ api/v2010/message/create-default/create-default.6.x.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 
 
 # Your Account Sid and Auth Token from twilio.com/console
-account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-auth_token = 'your_auth_token'
+account_sid = 'AC4a73d0d38c01eba4bb0e91259c7b19c7'
+auth_token = 'c7e8bfe73c5ec031617ad3d54dafe089'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
